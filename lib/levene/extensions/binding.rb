28d92c1e0564519c5861ccf74fa91763a8329bf1
Rob Eanes <robeanes@capitalthought.com>
2011-03-24 12:11:15 -0500

    factor out fault handling to common call_api method adds delete wrapper

--- lib/levene/extensions/binding.rb
+++ lib/levene/extensions/binding.rb
@@ -7,56 +7,54 @@ module Levene
         @session_id = nil
       end
 
-      def describe(model)
-        result = call_remote(:describeSObject, :sObjectType => model.name.demodulize)
+      def call_api(*args)
+        result = call_remote(*args)
         if result[:Fault]
           raise result[:Fault][:faultstring]
         else
-          return result[:describeSObjectResponse][:result][:fields]
+          return result
         end
+      end
+
+      def describe(model)
+        result = call_api(:describeSObject, :sObjectType => model.name.demodulize)
+        return result[:describeSObjectResponse][:result][:fields]
       end
 
       def query(model, conditions)
         full_query = "SELECT #{model.columns.collect {|c| c[:name]}.join(", ")} FROM #{model.model_name.demodulize} #{conditions || ""}"
-        result = call_remote(:query, :queryString => full_query)
-        if result[:Fault]
-          raise result[:Fault][:faultstring]
-        else
-          result[:queryResponse][:result].nil? ? [] : [result[:queryResponse][:result][:records]].flatten
-        end
+        result = call_api(:query, :queryString => full_query)
+        result[:queryResponse][:result].nil? ? [] : [result[:queryResponse][:result][:records]].flatten
       end
 
       def create(model)
         return false unless model.valid?
-        result = call_remote(:create, model.to_s_object(:creatable))
-        if result[:Fault]
-          raise result[:Fault][:faultstring]
+        result = call_api(:create, model.to_s_object(:creatable))
+        unless result[:createResponse][:result][:id].nil?
+          model.id = result[:createResponse][:result][:id]
+          return true
         else
-          unless result[:createResponse][:result][:id].nil?
-            model.id = result[:createResponse][:result][:id]
-            return true
-          else
-            model.errors.add_to_base(result[:createResponse][:result][:errors][:message])
-            return false
-          end
+          model.errors.add_to_base(result[:createResponse][:result][:errors][:message])
+          return false
         end
       end
 
       def update(model)
         return false unless model.valid?
-        result = call_remote(:update, model.to_s_object(:updatable))
-        if result[:Fault]
-          raise result[:Fault][:faultstring]
+        result = call_api(:update, model.to_s_object(:updatable))
+        if result[:updateResponse][:result][:success] == "false"
+          model.errors.add_to_base(result[:updateResponse][:result][:errors][:message])
+          return false
         else
-          debugger
-          if result[:updateResponse][:result][:success] == "false"
-            model.errors.add_to_base(result[:updateResponse][:result][:errors][:message])
-            return false
-          else
-            return true
-          end
+          return true
         end
       end
+      
+      def delete(models)
+        ids = models.collect { |m| [:ids, m.id] }.flatten
+        result = call_api(:delete, ids)
+      end
+      
     end
   end
 end
